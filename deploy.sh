1763919394704ecc4de8722c0aa3f6dd73e77e89
Mario Kaiser <ehmprah@localhost.localdomain>
2019-10-21 10:00:36 +0200

    Finally switch to SSH deploy

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/ehmprah/frgmnts.blog.git master:gh-pages
+git push -f git@github.com:ehmprah/frgmnts.blog.git master:gh-pages
 
 cd -
